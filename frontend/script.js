2ad3050a49103fcbb4d0e70799297f256de7c9f1
roshan-codes22 <roshank221199@gmail.com>
2025-04-09 08:41:16 -0400

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         try {
-            const response = await fetch('https://case-studies-generator-4ol1.vercel.app/', {
+            const response = await fetch('https://case-study-backend-w20q.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
